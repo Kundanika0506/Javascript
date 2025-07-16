f45f3daf433692668bf8eacf6cc5c907321708d9
Kundanika0506 <kundanika.madireddy@gmail.com>
2025-07-16 19:42:47 -0400

    sort elements in an array

---
+++ SortelementinArray.js
@@ -0,0 +1,9 @@
+// ascending order
+const arr = [4,6,8,7,9,2,3,12,16,14,11,10,5,1]
+arr.sort((a,b)=> a-b)
+console.log(arr)
+
+// Descending order
+const arr2 = [1,2,3,4,5,6,7,8,9,0]
+arr2.sort((a,b)=> b-a)
+console.log(arr2)
